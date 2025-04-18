9443c513a2d4b1d5f0ab1b519a19202e6ef28f84
AgustinMadygraf <agustin.mtto.madygraf>
2025-04-18 13:40:24 -0300

    Add run_flask.py: initialize MainApp with logging and production configuration

--- run_flask.py
+++ run_flask.py
@@ -5,12 +5,11 @@ Path: run.py
 
 from src.main import MainApp
 from src.utils.logging.simple_logger import LoggerService
-from src.config.production import ProductionConfig  # Importa la configuración de producción si es necesario
+from src.config.production import ProductionConfig
 
 if __name__ == "__main__":
     try:
         logger = LoggerService()
-        # Cambia ProductionConfig por DefaultConfig según el entorno deseado
         main_app = MainApp(logger, config_object=ProductionConfig)
         main_app.run()
     except KeyboardInterrupt:
